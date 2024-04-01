504a2bbc1c10002e712ada8a8bab344083ead5c5
Dead4light <129154283+Dead4light@users.noreply.github.com>
2024-04-01 23:18:38 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HexadecimalMultiplicationTable
+{
+    class HexadecimalMultiplicationTable // Змінено ім'я класу на відповідне ім'я вихідного файлу програми
+    {
+        static void Main(string[] args)
+        {
+            // Виведення заголовків рядків та стовпців
+            Console.Write("    ");
+            for (int i = 0; i <= 15; i++)
+            {
+                Console.Write($"{i:X}  ");
+            }
+            Console.WriteLine();
+
+            // Виведення рядків таблиці множення
+            for (int i = 0; i <= 15; i++)
+            {
+                Console.Write($"{i:X}  ");
+                for (int j = 0; j <= 15; j++)
+                {
+                    Console.Write($"{i * j:X}  ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
